5597798225fefd8a71f316c9f7821474db71ac35
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-05-29 01:50:20 +0330

    refactor(event): add BaseEventDto

---
+++ Application/Events/DTOs/BaseEventDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Events.DTOs;
+
+public class BaseEventDto
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public bool isCancelled { get; set; }
+    public string City { get; set; } = string.Empty;
+    public string Venue { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
--- Application/Events/DTOs/CreateEventDto.cs
+++ Application/Events/DTOs/CreateEventDto.cs
@@ -10,17 +10,8 @@ namespace Application.Events.DTOs
     /// the client is allowed to provide. Server-managed fields
     /// like Id and CreatedAt are intentionally excluded.
     /// </summary>
-    public class CreateEventDto
+    public class CreateEventDto : BaseEventDto
     {
-        public string Title { get; set; } = string.Empty;
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string Description { get; set; } = string.Empty;
-        public string Category { get; set; } = string.Empty;
-        public bool isCancelled { get; set; }
-        public string City { get; set; } = string.Empty;
-        public string Venue { get; set; } = string.Empty;
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+
     }
 }

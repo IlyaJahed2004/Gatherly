8eae2e95fe290f5187cc6208810de8212587e50c
@raminbuzarpur <raminbuzarpur@gmail.com>
2026-06-08 02:33:41 +0330

    style: extend the active tab underline to be much wider (250px)

--- client-app/src/components/Navbar/index.tsx
+++ client-app/src/components/Navbar/index.tsx
@@ -4,7 +4,7 @@ import Button from '../Button';
 const Navbar = () => {
   return (
     <nav className="bg-[#F3F4F6] py-8 px-8 md:px-16 flex items-center justify-between border-b border-gray-200">
-      {/* Logo: 48px, #078c80, Gveret Levin */}
+      {/* Logo */}
       <div 
         className="text-[48px] font-normal text-[#078C80] tracking-tight cursor-pointer" 
         style={{ fontFamily: '"Gveret Levin", cursive' }}
@@ -12,15 +12,15 @@ const Navbar = () => {
         Gatherly
       </div>
 
-      {/* Tabs: 24px, Medium */}
-      <div className="hidden md:flex items-center gap-24">
+      {/* Tabs */}
+      <div className="hidden md:flex items-center gap-24 relative">
         {/* Active Tab */}
         <div className="relative flex flex-col items-center">
           <a href="/" className="text-[#078C80] text-[24px] font-medium pb-2">
             home
           </a>
-          {/* Centered Extended Underline */}
-          <div className="absolute -bottom-[1px] w-[160px] left-1/2 -translate-x-1/2 h-[3px] bg-[#078C80]"></div>
+          {/* خط بسیار کشیده‌تر (250px) و دقیقاً وسط‌چین شده */}
+          <div className="absolute -bottom-[1px] w-[250px] left-1/2 -translate-x-1/2 h-[4px] bg-[#078C80]"></div>
         </div>
         
         {/* Inactive Tab */}

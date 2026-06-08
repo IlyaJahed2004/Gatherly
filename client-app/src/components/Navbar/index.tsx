69eef3f1dd7f1cf27a4c2442a3c474834c814530
@raminbuzarpur <raminbuzarpur@gmail.com>
2026-06-08 03:41:43 +0330

    style: implement perfect extended underline using tailwind after pseudo-element

--- client-app/src/components/Navbar/index.tsx
+++ client-app/src/components/Navbar/index.tsx
@@ -12,22 +12,27 @@ const Navbar = () => {
         Gatherly
       </div>
 
-      {/* Tabs - فاصله (Gap) دقیقاً 100 پیکسل تنظیم شد */}
+      {/* Tabs - فاصله بین تب‌ها */}
       <div className="hidden md:flex items-center gap-[100px]">
+        
         {/* Active Tab */}
-        <div className="relative flex justify-center items-center">
-          {/* پدینگ پایین (pb-4) اضافه شد تا فاصله خط از متن استاندارد شود */}
-          <a href="/" className="text-[#078C80] text-[24px] font-medium pb-4">
-            home
-          </a>
-          {/* عرض خط به 120 پیکسل کاهش یافت تا دقیقاً منطبق با طرح فیگما شود */}
-          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[120px] h-[4px] bg-[#078C80]"></div>
-        </div>
+        {/* ترفند جادویی: استفاده از after: برای ساختن خطی که کاملاً مستقل از کادر کلمه است */}
+        <a 
+          href="/" 
+          className="relative text-[#078C80] text-[24px] font-medium pb-2
+          after:content-[''] after:absolute after:-bottom-2 after:left-1/2 after:-translate-x-1/2 after:w-[150px] after:h-[4px] after:bg-[#078C80]"
+        >
+          home
+        </a>
         
         {/* Inactive Tab */}
-        <a href="/about" className="text-[#078C80] text-[24px] font-medium pb-4 hover:opacity-80 transition-opacity">
+        <a 
+          href="/about" 
+          className="relative text-[#078C80] text-[24px] font-medium pb-2 hover:opacity-80 transition-opacity"
+        >
           About us
         </a>
+
       </div>
 
       {/* Auth Button */}

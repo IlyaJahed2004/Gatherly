d6f01ae6f7c44b39b4ca17cb7d1761ee07e06e32
mhfathali <mhfathali2003@gmail.com>
2026-07-05 13:00:10 +0330

    feat(routes): add /events/:id/edit route for EditEventPage

--- client-app/src/routes/index.tsx
+++ client-app/src/routes/index.tsx
@@ -4,11 +4,13 @@ import ProfilePage from '../pages/ProfilePage';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import CreateEventPage from '../pages/CreateEventPage';
+import EditEventPage from '../pages/EditEventPage';
 
 export const layoutRoutes = [
   { path: '/',                  index: true,  element: <HomePage /> },
   { path: '/events/:id',        index: false, element: <EventDetailPage /> },
   { path: '/events/create',     index: false, element: <CreateEventPage /> },
+  { path: '/events/:id/edit',   index: false, element: <EditEventPage /> },
   { path: '/profile/:id', index: false, element: <ProfilePage /> },
 ];
 
